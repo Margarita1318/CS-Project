8e1fb59304246fbd59d45d8cf930d71d12489c41
Margarita1318 <mt667@njit.edu>
2024-11-21 22:05:39 -0500

    Update script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log("InfiniBuilds:");
+for (let i = 1; i <= 3; i++) {
+  console.log("Iteration:", i);
+}
+
+console.log("\ntoUpperCase():", str.toUpperCase());
+console.log("toLowerCase():", str.toLowerCase());
